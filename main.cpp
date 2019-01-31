cea86e0c4d52fde3eba5d9b9538ca5c6fac05cba
Alexandr <wh1ted0g@ya.ru>
2019-01-31 18:41:22 +0500

    Update

--- main.cpp
+++ main.cpp
@@ -29,40 +29,85 @@
 #include <tlhelp32.h>
 #include <conio.h>
 #include <ctime>
+
 using namespace std;
+
+HANDLE hSnap;
+PROCESSENTRY32 proc;
+DWORD PID;
+
+time_t rawtime;
+struct tm * time_start;
+
+ifstream fin;
+ofstream fout;
+
+string* arr_names;
+string* arr_names_temp;
+string temp;
+string sOldExeFile;
+
+unsigned int* arr_seconds;
+unsigned int* arr_seconds_temp;
+unsigned int int_seconds = 0;
+
+unsigned short count_execs = 0;
+unsigned short past_active_in_arr = 0;
+
+char szTitle[16] = {0};
+
+bool bFirstProcess = true;
+bool run = true;
+
+void saveOnHDD() {
+
+    unsigned short hours;
+    unsigned short minutes;
+    unsigned short seconds;
+
+    fout.open(".\\stats\\history.txt", std::ios::app);
+
+    fout << time_start->tm_year + 1900 << "/"
+        << time_start->tm_mon + 1 << "/"
+        << time_start->tm_mday << " "
+        << time_start->tm_hour << ":"
+        << time_start->tm_min << ":"
+        << time_start->tm_sec << " - ";
+
+    time_t rawtime2;
+    struct tm * time_end;
+    time(&rawtime2);
+    time_end = localtime(&rawtime2);
+
+
+    fout << time_end->tm_year + 1900 << "/"
+        << time_end->tm_mon + 1 << "/"
+        << time_end->tm_mday << " "
+        << time_end->tm_hour << ":"
+        << time_end->tm_min << ":"
+        << time_end->tm_sec << "\n\n";
+
+        localtime(&rawtime);
+
+    for(int i = 0; i < count_execs; i++) {
+
+        hours = arr_seconds[i] / 3600;
+        minutes = (arr_seconds[i] - hours * 3600) / 60;
+        seconds = arr_seconds[i] - (hours * 3600 + minutes * 60);
+
+        fout << "\t" << hours << ":" << minutes << ":" << seconds << "\t" << arr_names[i] << "\n";
+
+    }
+
+    fout << "\n";
+
+    fout.close();
+}
 
 int main()
 {
-
-    HANDLE hSnap;
-    PROCESSENTRY32 proc;
-    DWORD PID;
-
-    time_t rawtime;
-    struct tm * time_start;
-    struct tm * time_end;
     time ( &rawtime );
     time_start = localtime ( &rawtime );
-
-    ifstream fin;
-    ofstream fout;
-
-    string* arr_names;
-    string* arr_names_temp;
-    string temp;
-    string sOldExeFile;
-
-    unsigned int* arr_seconds;
-    unsigned int* arr_seconds_temp;
-    unsigned int int_seconds = 0;
-
-    unsigned short count_execs = 0;
-    unsigned short past_active_in_arr = 0;
-
-    char szTitle[16] = {0};
-
-    bool bFirstProcess = true;
-    bool run = true;
 
     arr_names = new string[count_execs];
     arr_names_temp = new string[count_execs];
@@ -72,7 +117,30 @@ int main()
 
     system("title Timerapp");
 
-    printf("Timerapp \nSPACE = PAUSE; ESCAPE = EXIT\n");
+    printf("TIMERAPP v2.1 \t (https://github.com/born-to-die)\n\
+           \nSPACE = PAUSE\
+           \nENTER = SESSION INFO\
+           \nBACKSPACE = HARD SAVE\
+           \nESCAPE = EXIT\n\n");
+
+    if (CreateDirectory(".\\apps", NULL))
+        printf("Created folder 'apps'\n");
+    else
+        printf("Folder 'apps' already have\n");
+
+
+    if (CreateDirectory(".\\stats", NULL))
+        printf("Created folder 'stats'\n");
+    else
+        printf("Folder 'stats' already have\n");
+
+    printf("\nSession start: %d/%d/%d %d:%d:%d\n",
+                    time_start->tm_year + 1900,
+                    time_start->tm_mon + 1,
+                    time_start->tm_mday,
+                    time_start->tm_hour,
+                    time_start->tm_min,
+                    time_start->tm_sec);
 
     while(run) {
 
@@ -98,7 +166,7 @@ int main()
 
                     if(temp != sOldExeFile) {
 
-                        printf("Current: %s\t%s\n", proc.szExeFile, szTitle);
+                        printf("\nCurrent: %s\t%s\n", proc.szExeFile, szTitle);
 
                         bool not_found = true;
 
@@ -163,9 +231,6 @@ int main()
                 unsigned short minutes;
                 unsigned short seconds;
 
-                time ( &rawtime );
-                time_end = localtime ( &rawtime );
-
                 fout.open(".\\stats\\history.txt", std::ios::app);
 
                 fout << time_start->tm_year + 1900 << "/"
@@ -175,12 +240,15 @@ int main()
                     << time_start->tm_min << ":"
                     << time_start->tm_sec << " - ";
 
-                fout << time_end->tm_year + 1900 << "/"
-                    << time_end->tm_mon + 1 << "/"
-                    << time_end->tm_mday << " "
-                    << time_end->tm_hour << ":"
-                    << time_end->tm_min << ":"
-                    << time_end->tm_sec << "\n\n";
+                time ( &rawtime );
+                time_start = localtime ( &rawtime );
+
+                fout << time_start->tm_year + 1900 << "/"
+                    << time_start->tm_mon + 1 << "/"
+                    << time_start->tm_mday << " "
+                    << time_start->tm_hour << ":"
+                    << time_start->tm_min << ":"
+                    << time_start->tm_sec << "\n\n";
 
                 for(int i = 0; i < count_execs; i++) {
 
@@ -188,7 +256,7 @@ int main()
                     minutes = (arr_seconds[i] - hours * 3600) / 60;
                     seconds = arr_seconds[i] - (hours * 3600 + minutes * 60);
 
-                    fout << "\t" << arr_names[i] << " - " << hours << ":" << minutes << ":" << seconds << "\n";
+                    fout << "\t" << hours << ":" << minutes << ":" << seconds << "\t" << arr_names[i] << "\n";
 
                 }
 
@@ -231,10 +299,69 @@ int main()
                 run = false;
                 break;
             }
+            case 13: {
+
+
+
+                printf("\n%d/%d/%d %d:%d:%d - ",
+                    time_start->tm_year + 1900,
+                    time_start->tm_mon + 1,
+                    time_start->tm_mday,
+                    time_start->tm_hour,
+                    time_start->tm_min,
+                    time_start->tm_sec);
+
+                time_t rawtime2;
+                struct tm * time_end;
+                time(&rawtime2);
+                time_end = localtime(&rawtime2);
+
+
+                printf("%d/%d/%d %d:%d:%d\n\n",
+                    time_end->tm_year + 1900,
+                    time_end->tm_mon + 1,
+                    time_end->tm_mday,
+                    time_end->tm_hour,
+                    time_end->tm_min,
+                    time_end->tm_sec);
+
+                localtime(&rawtime);
+
+                unsigned short hours;
+                unsigned short minutes;
+                unsigned short seconds;
+
+                for(int i = 0; i < count_execs; i++) {
+
+                    hours = arr_seconds[i] / 3600;
+                    minutes = (arr_seconds[i] - hours * 3600) / 60;
+                    seconds = arr_seconds[i] - (hours * 3600 + minutes * 60);
+
+                    printf("\t%d:%d:%d\t%s\n",
+                        hours,
+                        minutes,
+                        seconds,
+                        arr_names[i].c_str()
+                    );
+
+                }
+
+                break;
+            }
+            case 8: {
+
+                saveOnHDD();
+
+                printf("\nSession information has been saved.\n");
+
+                break;
+            }
             case 32: {
-                printf("PAUSE\n");
+
+                printf("\nPAUSE\n");
                 getch();
-                printf("PLAY\n");
+                printf("PLAY\n\n");
+
                 break;
             }
         }
